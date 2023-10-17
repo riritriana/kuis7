2ebf9045897d81dc0ecf56631e7ecf59d4e3f2e5
Riri Triana <riritriana21@gmail.com>
2023-10-17 20:36:40 +0700

    kuis

---
+++ main.js
@@ -0,0 +1,4 @@
+import{tambah,kali, phi} from "./mathOperation.js";
+console.log("penjumlahan :"+ tambah(9,8));
+console.log("perkalian : " + kali(8,7));
+console.log(" Nilai PI :" + phi(3.14159));
---
+++ mathOperation.js
@@ -0,0 +1,9 @@
+export function tambah(a,b){
+    return a+b;
+}
+export function kali(x,y){
+    return x*y;
+}
+export function phi(phi){
+    return phi;
+}
